738f55bf525ae0065e225fea56f3056174e3ff90
unknown <matthewaustinbell@gmail.com>
2019-11-10 18:45:28 -0600

    added props

--- KandyMarket.Data/Candy.cs
+++ KandyMarket.Data/Candy.cs
@@ -2,5 +2,8 @@
 {
     public class Candy
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Color { get; set; }
     }
 }
---
+++ KandyMarket.Data/CandyProvider.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+using Dapper;
+
+namespace KandyMarket.Data
+{
+    public class CandyProvider : ICandyProvider
+    {
+        private readonly string connectionString;
+
+        public IEnumerable<Candy> Get()
+        {
+            IEnumerable<Candy> candy = null;
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                candy = connection.Query<Candy>(
+                    "select id, name, color from Candy");
+            }
+
+            return candy;
+        }
+    }
+}
